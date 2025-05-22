434aef1284b4f5435bc5fe64cd166cea221300db
Eyal Hartman <eyal@example.com>
2025-05-22 15:45:45 +0300

    Part 2 and 3, Good luck for us

--- src/ascii_art/Shell.java
+++ src/ascii_art/Shell.java
@@ -17,7 +17,7 @@ public class Shell {
 
 	private static final String EXIT_MSG = "exit";
 	private static final String CHARS_MSG = "chars";
-	private static final Character[] DEFAULT_CHAR_SET = {'0','1','2','3','4','5','6','7','8','9'};
+	private static final Character[] DEFAULT_CHAR_SET = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	private static final int DEFAULT_RES = 2;
 	private static final String ADD_MSG = "add";
 	private static final char SPACE_CHAR = ' ';
@@ -62,130 +62,153 @@ public class Shell {
 	private boolean asciiAlgoDirty = true;
 
 
-	public Shell(){
+	public Shell() {
 		this.charset = new TreeSet<>(Arrays.asList(DEFAULT_CHAR_SET));
 		this.res = DEFAULT_RES;
 		this.roundingMode = RoundingMode.NEAREST;
+		char[] arrChar = toCharArray(charset);
+		this.matcher = new SubImgCharMatcher(arrChar);
 	}
 
 
 	//todo add exceptions
-	public void run(String imageName) {
-		if (extractImg(imageName)) return;
+	public void run(String imageName) throws IOException {
+		extractImg(imageName);
 
 		while (true) {
-			System.out.print(">>> ");
-			String action = KeyboardInput.readLine();
+			try {
 
-			if (action.startsWith(EXIT_MSG))  break;
-			else if (action.startsWith(CHARS_MSG))  {chars_cmd();matcherDirty = true;}
-			else if (action.startsWith(ADD_MSG)) {add_cmd(action); matcherDirty = true;}
-			else if (action.startsWith(REMOVE_MSG)) {remove_cmd(action); matcherDirty = true;}
-			else if (action.startsWith(RES_MSG)) resCmd(action);
-			else if (action.startsWith(OUTPUT_MSG)) outputCmd(action);
-			else if (action.startsWith(ASCII_MSG)) asciiCmd(action);
-			else if (action.startsWith(ROUND_MSG)) roundCmd(action);
-			else System.out.println(INCORRECT_OUTPUT_FORMAT_MSG);
+				System.out.print(">>> ");
+				String action = KeyboardInput.readLine();
+
+				if (action.startsWith(EXIT_MSG)) break;
+				else if (action.startsWith(CHARS_MSG)) {
+					chars_cmd();
+					matcherDirty = true;
+				} else if (action.startsWith(ADD_MSG)) {
+					add_cmd(action);
+					matcherDirty = true;
+				} else if (action.startsWith(REMOVE_MSG)) {
+					remove_cmd(action);
+					matcherDirty = true;
+				} else if (action.startsWith(RES_MSG)) resCmd(action);
+				else if (action.startsWith(OUTPUT_MSG)) outputCmd(action);
+				else if (action.startsWith(ASCII_MSG)) asciiCmd();
+				else if (action.startsWith(ROUND_MSG)) roundCmd(action);
+				else {
+					throw new IOException(INCORRECT_OUTPUT_FORMAT_MSG);
+				}
+			}
+			catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
 		}
 
 	}
 
-	private boolean extractImg(String imageName) {
-		try {
-			this.image = new Image(imageName);
-			asciiAlgoDirty = true;
-			int imgWidth = image.getWidth();
-			int imgHeight = image.getHeight();
-
-			this.maxCharsInRow = imgWidth;
-			this.minCharsInRow = Math.max(1, imgWidth / imgHeight);
-			this.res = DEFAULT_RES;
-		} catch (IOException e) {
-			System.out.println("Image not found.");
-			return true;
-		}
-		return false;
+	private void extractImg(String imageName) throws IOException {
+		this.image = new Image(imageName);
+		asciiAlgoDirty = true;
+		int imgWidth = image.getWidth();
+		int imgHeight = image.getHeight();
+		this.maxCharsInRow = imgWidth;
+		this.minCharsInRow = Math.max(1, imgWidth / imgHeight);
+		this.res = DEFAULT_RES;
 	}
 
-	private void remove_cmd(String action) {
+	private void remove_cmd(String action) throws IOException {
 
 		String[] parts = action.split(" ");
 		if (parts.length < 2) {
-			System.out.println(INCORRECT_ADD_MSG);
-			return;
+			throw new IOException(INCORRECT_REMOVE_MSG);
 		}
 		String specificRemove = parts[1];
 
 		switch (specificRemove) {
-			case ALL_MSG :
-				for (char i = FIRST_ASCII_INDEX; i < AFTER_LAST_ASCII_INDEX; i++)  this.charset.remove(i);
+			case ALL_MSG:
+				for (char i = FIRST_ASCII_INDEX; i < AFTER_LAST_ASCII_INDEX; i++) {
+					this.charset.remove(i);
+					matcher.removeChar(i);
+				}
 				return;
 			case SPACE_MSG:
 				this.charset.remove(SPACE_CHAR);
+				matcher.removeChar(SPACE_CHAR);
 				return;
 		}
 
-		if (specificRemove.length()== 1) {
+		if (specificRemove.length() == 1) {
 			char charToRemove = specificRemove.charAt(0);
-			if ( charToRemove> FIRST_ASCII_INDEX && charToRemove < AFTER_LAST_ASCII_INDEX) {
+			if (charToRemove > FIRST_ASCII_INDEX && charToRemove < AFTER_LAST_ASCII_INDEX) {
 				this.charset.remove(specificRemove.charAt(FIRST_INDEX));
+				matcher.removeChar(charToRemove);
 			}
+			return;
 		}
 
-		if (specificRemove.charAt(1)== HYPHEN_CHAR){
+		if (specificRemove.charAt(1) == HYPHEN_CHAR) {
 			char charARemove = specificRemove.charAt(0);
 			char charBRemove = specificRemove.charAt(2);
-			if (charARemove>FIRST_ASCII_INDEX &&
-					charARemove<AFTER_LAST_ASCII_INDEX &&
-					charBRemove>FIRST_ASCII_INDEX &&
-					charBRemove<AFTER_LAST_ASCII_INDEX) {
-				char charStart = (char)Math.min(charARemove, charBRemove), charEnd =
-						(char)Math.max(charARemove, charBRemove);
-				for (char i = charStart; i<=charEnd;i++) this.charset.remove(i);
+			if (charARemove > FIRST_ASCII_INDEX &&
+					charARemove < AFTER_LAST_ASCII_INDEX &&
+					charBRemove > FIRST_ASCII_INDEX &&
+					charBRemove < AFTER_LAST_ASCII_INDEX) {
+				char charStart = (char) Math.min(charARemove, charBRemove), charEnd =
+						(char) Math.max(charARemove, charBRemove);
+				for (char i = charStart; i <= charEnd; i++) {
+					this.charset.remove(i);
+					matcher.removeChar(i);
+				}
 			}
-		}
-		else{
-			System.out.println(INCORRECT_REMOVE_MSG);
+		} else {
+			throw new IOException(INCORRECT_REMOVE_MSG);
 		}
 	}
 
-	private void add_cmd(String action) {
+	private void add_cmd(String action) throws IOException {
 		String[] parts = action.split(" ", 2);
 		if (parts.length < 2) {
-			System.out.println(INCORRECT_ADD_MSG);
-			return;
+			throw new IOException(INCORRECT_ADD_MSG);
 		}
 		String specificAdd = parts[1];
 		switch (specificAdd) {
-			case ALL_MSG :
-				for (char i = FIRST_ASCII_INDEX; i < AFTER_LAST_ASCII_INDEX; i++)  this.charset.remove(i);
+			case ALL_MSG:
+				for (char i = FIRST_ASCII_INDEX; i < AFTER_LAST_ASCII_INDEX; i++) {
+					this.charset.remove(i);
+					matcher.addChar(i);
+				}
 				return;
 			case SPACE_MSG:
 				this.charset.add(SPACE_CHAR);
+				matcher.addChar(SPACE_CHAR);
 				return;
 		}
 
-		if (specificAdd.length()== 1) {
+		if (specificAdd.length() == 1) {
 			char charToAdd = specificAdd.charAt(0);
-			if ( charToAdd> FIRST_ASCII_INDEX && charToAdd < AFTER_LAST_ASCII_INDEX) {
+			if (charToAdd > FIRST_ASCII_INDEX && charToAdd < AFTER_LAST_ASCII_INDEX) {
 				this.charset.add(specificAdd.charAt(FIRST_INDEX));
+				matcher.addChar(charToAdd);
+				return;
 			}
 		}
 
-		if (specificAdd.charAt(1)== HYPHEN_CHAR){
+		if (specificAdd.charAt(1) == HYPHEN_CHAR) {
 			char charAAdd = specificAdd.charAt(0);
 			char charBAdd = specificAdd.charAt(2);
-			if (charAAdd>FIRST_ASCII_INDEX &&
-					charAAdd<AFTER_LAST_ASCII_INDEX &&
-					charBAdd>FIRST_ASCII_INDEX &&
-					charBAdd<AFTER_LAST_ASCII_INDEX) {
-				char charStart = (char)Math.min(charAAdd, charBAdd), charEnd =
-						(char)Math.max(charAAdd, charBAdd);
-				for (char i = charStart; i<=charEnd;i++) this.charset.add(i);
+			if (charAAdd > FIRST_ASCII_INDEX &&
+					charAAdd < AFTER_LAST_ASCII_INDEX &&
+					charBAdd > FIRST_ASCII_INDEX &&
+					charBAdd < AFTER_LAST_ASCII_INDEX) {
+				char charStart = (char) Math.min(charAAdd, charBAdd), charEnd =
+						(char) Math.max(charAAdd, charBAdd);
+				for (char i = charStart; i <= charEnd; i++) {
+					this.charset.add(i);
+					matcher.addChar(i);
+				}
 			}
-		}
-		else{
-			System.out.println(INCORRECT_ADD_MSG);
+		} else {
+			throw new IOException(INCORRECT_ADD_MSG);
 		}
 	}
 
@@ -198,7 +221,7 @@ public class Shell {
 		}
 	}
 
-	private void resCmd(String action) {
+	private void resCmd(String action) throws IOException {
 		String param = action.substring(AFTER_RES_INDEX).trim();
 
 		if (param.isEmpty()) {
@@ -212,12 +235,11 @@ public class Shell {
 		} else if (param.startsWith("down")) {
 			newRes = res / 2;
 		} else {
-			System.out.println(INCORRECT_RES_FORMAT_MSG);
-			return;
+			throw new IOException(INCORRECT_REMOVE_MSG);
 		}
 
 		if (newRes < minCharsInRow || newRes > maxCharsInRow) {
-			System.out.println(INCORRECT_RES_BOUND_MSG);
+			throw new IOException(INCORRECT_REMOVE_MSG);
 		} else {
 			res = newRes;
 			asciiAlgoDirty = true;
@@ -225,43 +247,42 @@ public class Shell {
 		}
 	}
 
-	private void roundCmd(String action) {
+	private void roundCmd(String action) throws IOException {
 		String param = action.substring(ROUND_MSG.length()).trim();
 		switch (param) {
-			case "up" ->{
+			case "up" -> {
 				this.roundingMode = RoundingMode.UP;
 				if (matcher != null) matcher.setRoundingMode(this.roundingMode);
 			}
-			case "down" ->{
+			case "down" -> {
 				this.roundingMode = RoundingMode.DOWN;
 				if (matcher != null) matcher.setRoundingMode(this.roundingMode);
 			}
-			case "abs" ->{
+			case "abs" -> {
 				this.roundingMode = RoundingMode.NEAREST;
 				if (matcher != null) matcher.setRoundingMode(this.roundingMode);
 			}
 			default -> {
-				System.out.println(INCORRECT_ROUNDING_MODE_MSG);
+				throw new IOException(INCORRECT_ROUNDING_MODE_MSG);
 			}
 		}
 
 	}
 
-	private void outputCmd(String action) {
+	private void outputCmd(String action) throws IOException {
 		String output = action.substring(OUTPUT_MSG.length()).trim();
 		if (output.startsWith("html")) {
 			this.output = OUTPUT_OPTIONS[0];
 		} else if (output.startsWith("console")) {
 			this.output = OUTPUT_OPTIONS[1];
 		} else {
-			System.out.println(INCORRECT_OUTPUT_MSG);
+			throw new IOException(INCORRECT_OUTPUT_MSG);
 		}
 	}
 
-	private void asciiCmd(String action) {
+	private void asciiCmd() throws IOException {
 		if (charset.size() < 2) {
-			System.out.println(INCORRECT_ASCII_MSG);
-			return;
+			throw (new IOException(INCORRECT_ASCII_MSG));
 		}
 		char[] arrChar = toCharArray(charset);
 		if (matcherDirty) {
@@ -274,14 +295,14 @@ public class Shell {
 			this.asciiAlgo = new AsciiArtAlgorithm(this.image, res, matcher);
 			asciiAlgoDirty = false;
 		}
-		char[][] charset = asciiAlgo.run();
+		char[][] asciiPhoto = asciiAlgo.run();
 		if (Objects.equals(output, "html")) {
 			HtmlAsciiOutput htmlAsciiOutput = new HtmlAsciiOutput("output.html",
 					"Courier New");
-			htmlAsciiOutput.out(charset);
+			htmlAsciiOutput.out(asciiPhoto);
 		} else {
 			ConsoleAsciiOutput consoleAsciiOutput = new ConsoleAsciiOutput();
-			consoleAsciiOutput.out(charset);
+			consoleAsciiOutput.out(asciiPhoto);
 		}
 	}
 
@@ -294,7 +315,7 @@ public class Shell {
 		return a;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Shell shell = new Shell();
 		if (args.length == 0) {
 			System.out.println("Please provide an image file name.");
